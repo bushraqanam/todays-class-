33abcb79fc57115de26a99c19e0a87699cc4e2bd
bushraqanam <bqanam@gmail.com>
2024-08-07 16:16:42 -0500

    class 8

---
+++ main.js
@@ -0,0 +1,14 @@
+//combainding operators//
+//assingnment operators//
+var result = 10 + 2 * 3 / 2 + (5 * 3) - 8;
+//apply bodmas rule// b for bracket,o for open, d for division,m for multiplycation, a for addition, s for subraction
+console.log(result);
+//assingnment operators//
+var num = 9;
+num += 5;
+num -= 2;
+num *= 4;
+num /= 3;
+num = Math.pow(num, 2);
+num %= 2;
+console.log(num);
---
+++ main.ts
@@ -0,0 +1,15 @@
+//combainding operators//
+//assingnment operators//
+let result:number = 10+2*3/2+(5*3)-8
+//apply bodmas rule// b for bracket,o for open, d for division,m for multiplycation, a for addition, s for subraction
+console.log(result)
+//assingnment operators//
+let num:number = 9
+num +=5
+num -=2
+num *=4
+num /=3
+num **=2
+num %=2
+console.log(num)
+
